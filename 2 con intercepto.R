ba6d80a4d7fa1edf34ed15796c807eb38e666aa9
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-10-20 23:27:41 -0500

    Add files via upload

---
+++ 2 con intercepto.R
@@ -0,0 +1,45 @@
+z<-0
+tau<-0
+for(j in 1:10000){
+  z[1]<-0
+  for(i in 2:50){
+    z[i]<-z[i-1]+rnorm(1,0,1)}
+  
+  difZ<-diff(z)
+  zt_1<-z[1:49]
+  modelo<-summary(lm(difZ~zt_1))
+  pi<-modelo$coefficients[2,1]
+  desviacion<-modelo$coefficients[2,2]
+  tau[j]<-pi/(desviacion)
+} 
+tau
+quantile(tau, prob=c(0.01,0.05,0.1))
+#####
+library(ggplot2)
+
+library(gridExtra)
+
+tau<-as.data.frame(tau)
+h1<-ggplot(data = tau,
+           aes(x = tau, fill=cut)) +
+  geom_histogram(fill = "steelblue", color="black")+
+  theme_minimal()+
+  xlab("Tau") + 
+  ylab("Frecuencia")+ 
+  theme (axis.text.x = element_text(size=rel(1.3)),
+         axis.text.y = element_text(size=rel(1.3)))+
+  theme (axis.title = element_text(size=rel(1.7)));h1
+
+tstudent<-rt(10000,48)
+tstudent<-as.data.frame(tstudent)
+h2<-ggplot(data = tstudent,
+           aes(x = tstudent, fill=cut)) +
+  geom_histogram(fill = "steelblue", color="black")+
+  theme_minimal()+
+  xlab("T-Student") + 
+  ylab("Frecuencia")+ 
+  theme (axis.text.x = element_text(size=rel(1.3)),
+         axis.text.y = element_text(size=rel(1.3)))+
+  theme (axis.title = element_text(size=rel(1.7)));h2
+
+grid.arrange(h1, h2, ncol=2, nrow = 1)
